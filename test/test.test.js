1bdef5fb2ea7e617359654ce963ce917a7c80f2e
Alexandr Ostapenko <alexandr.amostap@gmail.com>
2017-07-25 12:34:34 +0300

    added first test

---
+++ test/test.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+
+describe('A Feature', () => {
+  describe('A Scenarion', () => {
+    it('Does someting expected', () => {
+      assert(true);
+    });
+  });
+});
+
+describe('dasdas', () => {
+  describe('A Scenarion', () => {
+    it('Does someting expected', () => {
+      assert(true);
+    });
+  });
+});
